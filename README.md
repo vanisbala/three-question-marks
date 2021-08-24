9ed29cbdb67bdf3585dbe89d7e8d9ff40ca5a2e0
vani <vanisbala@gmail.com>
2021-08-23 20:37:57 -0700

    updated the necessary code

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
 # three-question-marks
+It checks whether the given string contain three consequtive question marks that 
+appears in between a pair of two numbers with their sum value 10.
+
+## Description
+- checks whether there is a number pair with the sum value 10.
+- checks whether '???' in between those pairs.
+- It displays a message whether the string staisfies these conditions or not
+- Ex : eeer2gh?gh8  - The string did not contain the specific pattern
+       nf3k???d745e - The string contains the specific pattern
+
+## Installation
+git clone https://github.com/vanisbala/three-question-marks
+
+## Built with
+- [HTML](https://developer.mozilla.org/en-US/docs/Web/HTML)
+- [CSS](https://developer.mozilla.org/en-US/docs/Web/CSS)
+- [JavaScript](https://developer.mozilla.org/en-US/docs/Web/Javascript)
+
+## Deployed Link
+- [See live Site]( https://vanisbala.github.io/three-question-marks/)
+
+
+## Author
+### Vani 
+- [Link to Github](https://github.com/vanisbala)
+
+## Credits
+Solved the coding challenge from,
+- [medium.com] (https://medium.com/coderbyte/the-5-hardest-code-challenges-for-beginners-e410da4474b) 
+
+
+
+
---
+++ script.js
@@ -0,0 +1,86 @@
+
+//This program checks whether the given string contains '???' three consecutive question mark in between two numbers whose 
+// added value is ten.
+
+      
+      var checkstr;
+      var numArr= [];
+      var flagForThreeQs = false;
+      //console.log("Entered String:"+ checkstr);
+
+      function check(){
+      checkstr = document.getElementById('userStr').value;
+      console.log(checkstr);
+      numArr=checkNumbers(checkstr); // Array of Number,position pair
+      numArr.forEach(checkAddTen);
+      if (flagForThreeQs){
+        
+        document.getElementById('getResult').innerHTML = "Given string contains '???' in between the number pair which has the sum value 10";
+        document.getElementById('getResult').style.color = "limegreen";
+        document.getElementById('getResult').style.fontSize = "large";
+        document.getElementById('getResult').style.fontWeight="bold";
+        }
+        else{
+            document.getElementById('getResult').innerHTML = "Given string does not contains '???' in between the number pair which has the sum value 10";
+            document.getElementById('getResult').style.color = "firebrick";  
+            document.getElementById('getResult').style.fontSize = "large";
+            document.getElementById('getResult').style.fontWeight="bold";
+        }
+
+    }
+
+      // This function checks weather any of the number pair makes 10. if so it then calls checkQuestionMark function.
+      function checkAddTen(item,index,numArr){
+        if (numArr.length !== 0){
+          if( (index+1) < numArr.length){
+            var current = parseInt(item.value);
+            var next = parseInt(numArr[index+1].value);
+            console.log(typeof(next), next);
+            if( (current + next) === 10){
+              
+              checkQuestionMark(item.pos , numArr[index+1].pos);
+              
+            }
+            else{
+              console.log(`${item.value} and ${numArr[index+1].value} doesn't make 10`);
+            }
+          }
+          else{
+            console.log("No more value to compare")
+          }
+        }
+        else{
+          console.log("No numbers in the String")
+        }
+      }
+
+
+    
+      // This function checks weather there are ??? pattern exists in the substring obtained from the given first and second 
+      //position index. If so it turns the universal flag true;
+      function checkQuestionMark(first,second){
+        console.log(first,second,checkstr);
+        chksubstr = checkstr.slice(first,second+1);
+        console.log("substring :" + chksubstr);
+        let regex = /\?\?\?/gi;
+        if (regex.test(chksubstr)){
+          flagForThreeQs = true;  
+
+        }
+      }
+
+      //This function will check for numbers in the given string and push the numbers and their positions into the array object 
+      // with {value : number, pos: position} pairs
+      function checkNumbers(str) {
+        var len = str.length;
+        for (let i = 0; i < len; i++) {
+          var charAscii = str.charCodeAt(i);
+          console.log(`Ascii of ${str[i]} is ${charAscii}`);
+          if ((charAscii>47) && (charAscii < 58)){
+            numArr.push({ value: str[i], pos: i });
+          }
+        }
+        console.log(numArr);
+        return numArr;
+      }
+    
